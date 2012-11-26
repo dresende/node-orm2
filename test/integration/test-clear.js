e3fca6781da2e21038eff8874abeee5404f6ff16
Diogo Resende <dresende@thinkdigital.pt>
2012-11-26 19:52:20 +0000

    Adds Model.clear(), adds Model.clear() test, fixes bug when .find() returns no rows

---
+++ test/integration/test-clear.js
@@ -0,0 +1,35 @@
+var common     = require('../common');
+var assert     = require('assert');
+
+common.createConnection(function (err, db) {
+	db.driver.db.query([
+		"CREATE TEMPORARY TABLE `test_clear` (",
+		"`id` INT (5) NOT NULL PRIMARY KEY AUTO_INCREMENT,",
+		"`name` VARCHAR(100) NOT NULL",
+		")"
+	].join(""), function () {
+		db.driver.db.query("INSERT INTO `test_clear` VALUES (1, 'test'), (2, 'test')", function (err) {
+			if (err) throw err;
+
+			var TestModel = db.define('test_clear');
+
+			TestModel.find(function (err, Instances) {
+				assert.equal(err, null);
+				assert.equal(Array.isArray(Instances), true);
+				assert.equal(Instances.length, 2);
+
+				TestModel.clear(function (err) {
+					assert.equal(err, null);
+
+					TestModel.find(function (err, Instances) {
+						assert.equal(err, null);
+						assert.equal(Array.isArray(Instances), true);
+						assert.equal(Instances.length, 0);
+
+						db.close();
+					});
+				});
+			});
+		});
+	});
+});

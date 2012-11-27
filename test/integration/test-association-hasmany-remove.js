95c08e950cd7ade401a5401d8be742a9f5dc3563
Diogo Resende <dresende@thinkdigital.pt>
2012-11-27 18:03:56 +0000

    Fixes tests for remove* hasMany associations to use a table other than the one from other test to avoid problems in the future

--- test/integration/test-association-hasmany-remove.js
+++ test/integration/test-association-hasmany-remove.js
@@ -3,24 +3,24 @@ var assert     = require('assert');
 
 common.createConnection(function (err, db) {
 	db.driver.db.query([
-		"CREATE TEMPORARY TABLE `test_association_hasmany_get` (",
+		"CREATE TEMPORARY TABLE `test_association_hasmany_remove` (",
 		"`id` INT (5) NOT NULL PRIMARY KEY AUTO_INCREMENT,",
 		"`name` VARCHAR(100) NOT NULL",
 		")"
 	].join(""), function () {
 		db.driver.db.query([
-			"CREATE TEMPORARY TABLE `test_association_hasmany_get_assocs` (",
-			"`test_association_hasmany_get_id` INT (5) NOT NULL,",
+			"CREATE TEMPORARY TABLE `test_association_hasmany_remove_assocs` (",
+			"`test_association_hasmany_remove_id` INT (5) NOT NULL,",
 			"`assocs_id` INT(5) NOT NULL",
 			")"
 		].join(""), function () {
-			db.driver.db.query("INSERT INTO `test_association_hasmany_get` VALUES (1, 'test1'), (2, 'test2'), (3, 'test3')", function (err) {
+			db.driver.db.query("INSERT INTO `test_association_hasmany_remove` VALUES (1, 'test1'), (2, 'test2'), (3, 'test3')", function (err) {
 				if (err) throw err;
 
-				db.driver.db.query("INSERT INTO `test_association_hasmany_get_assocs` VALUES (1, 2), (1, 3)", function (err) {
+				db.driver.db.query("INSERT INTO `test_association_hasmany_remove_assocs` VALUES (1, 2), (1, 3)", function (err) {
 					if (err) throw err;
 
-					var TestModel = db.define('test_association_hasmany_get');
+					var TestModel = db.define('test_association_hasmany_remove');
 					TestModel.hasMany("assocs");
 
 					TestModel.get(1, function (err, Test1) {

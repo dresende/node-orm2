309e9c924529f3bb7a6ab1856dcb6d9b383653a7
Diogo Resende <dresende@thinkdigital.pt>
2012-11-29 10:19:08 +0000

    Updates test config example to show how to run single drivers and all drivers

--- test/config.example.js
+++ test/config.example.js
@@ -1,5 +1,11 @@
 // To test, rename this file to config.js and update
 // the following configuration
+//
+// To run a single driver, go to root folder and do (mysql example):
+// ORM_PROTOCOL=mysql node test/run
+//
+// To run all drivers:
+// make test
 
 exports.mysql = {
 	user     : "root",

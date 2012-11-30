9f28f7bed8d734bffda5ad72dfd0550a24876ead
Diogo Resende <dresende@thinkdigital.pt>
2012-11-30 11:35:16 +0000

    Updates tests for property types to include enum and object

--- test/integration/test-property-types.js
+++ test/integration/test-property-types.js
@@ -6,11 +6,16 @@ assert.equal(Property.check(String).type, "text");
 assert.equal(Property.check(Number).type, "number");
 assert.equal(Property.check(Boolean).type, "boolean");
 assert.equal(Property.check(Date).type, "date");
+assert.equal(Property.check(Object).type, "object");
+assert.equal(Property.check([ 'a', 'b' ]).type, "enum");
+assert.deepEqual(Property.check([ 'a', 'b' ]).values, [ 'a', 'b' ]);
 
 assert.equal({ type: "text" }.type, "text");
 assert.equal({ type: "number" }.type, "number");
 assert.equal({ type: "boolean" }.type, "boolean");
 assert.equal({ type: "date" }.type, "date");
+assert.equal({ type: "enum" }.type, "enum");
+assert.equal({ type: "object" }.type, "object");
 
 assert.throws(function () {
 	Property.check({ type: "unknown" });

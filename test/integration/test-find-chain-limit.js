dc7db2e9b79919aecfee703ce97e9ce4bb3aa533
Diogo Resende <dresende@thinkdigital.pt>
2013-01-23 12:00:39 +0000

    Fixes test-find-chain-limit table name

--- test/integration/test-find-chain-limit.js
+++ test/integration/test-find-chain-limit.js
@@ -2,14 +2,14 @@ var common     = require('../common');
 var assert     = require('assert');
 
 common.createConnection(function (err, db) {
-	common.createModelTable('test_find_limit', db.driver.db, function () {
-		common.insertModelData('test_find_limit', db.driver.db, [
+	common.createModelTable('test_find_chain_limit', db.driver.db, function () {
+		common.insertModelData('test_find_chain_limit', db.driver.db, [
 			{ id : 1, name : 'test1' },
 			{ id : 2, name : 'test2' }
 		], function (err) {
 			if (err) throw err;
 
-			var TestModel = db.define('test_find_limit', common.getModelProperties());
+			var TestModel = db.define('test_find_chain_limit', common.getModelProperties());
 
 			TestModel.find().limit(1).run(function (err, Instances) {
 				assert.equal(err, null);

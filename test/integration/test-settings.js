6afe28fb46862991aad9e88716d56b42b5ae98bd
Diogo Resende <dresende@thinkdigital.pt>
2013-01-25 15:03:46 +0000

    Fixes bug on settings.unset(), adds settings.get('foo.bar.*'), adds tests

--- test/integration/test-settings.js
+++ test/integration/test-settings.js
@@ -18,3 +18,14 @@ common.ORM.settings.unset("some....object", "some.sub.object");
 
 assert.equal(common.ORM.settings.get("some....object"), undefined);
 assert.equal(common.ORM.settings.get("some.sub.object"), undefined);
+
+common.ORM.settings.set("some.other.stuff", 123.45);
+common.ORM.settings.set("some.more.stuff", 123.45);
+common.ORM.settings.unset("some.*");
+
+assert.equal(common.ORM.settings.get("some.other.stuff"), undefined);
+assert.equal(common.ORM.settings.get("some.more.stuff"), undefined);
+assert.equal(typeof common.ORM.settings.get("some.*"), "object");
+assert.equal(Object.keys(common.ORM.settings.get("some.*")).length, 0);
+assert.equal(typeof common.ORM.settings.get("some"), "object");
+assert.equal(Object.keys(common.ORM.settings.get("some")).length, 0);

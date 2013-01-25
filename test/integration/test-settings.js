b5fc027bb9d4cfdeb6f24fa2afa6aad1036664ea
Diogo Resende <dresende@thinkdigital.pt>
2013-01-25 14:49:08 +0000

    Adds generic settings object test

---
+++ test/integration/test-settings.js
@@ -0,0 +1,15 @@
+var common     = require('../common');
+var assert     = require('assert');
+
+common.ORM.settings.set("some.sub.object", 123.45);
+
+assert.equal(common.ORM.settings.get("some.sub.object"), 123.45);
+assert.equal(common.ORM.settings.get("some.other.sub.object", 789), 789);
+
+var testFunction = function () {
+	return "test";
+};
+
+common.ORM.settings.set("some....object", testFunction);
+
+assert.equal(common.ORM.settings.get("some....object"), testFunction);

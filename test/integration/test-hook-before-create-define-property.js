5744ba29d9375de905d23baf2295d5ebc065fa20
Mark Roberts <markandrusroberts@gmail.com>
2013-03-14 12:34:52 -0500

    Adds test for Issue #69

---
+++ test/integration/test-hook-before-create-define-property.js
@@ -0,0 +1,26 @@
+var common     = require('../common');
+var assert     = require('assert');
+
+common.createConnection(function (err, db) {
+	common.createModelTable('test_hook_before_create_define_property', db.driver.db, function () {
+		var name = "Name was undefined";
+		var TestModel = db.define('test_hook_before_create_define_property', common.getModelProperties(), {
+			hooks: {
+				beforeCreate: function () {
+					if (!this.name) {
+						this.name = name;
+					}
+				}
+			}
+		});
+
+		var Test = new TestModel();
+		Test.save(function (err) {
+			assert.equal(err, null);
+			
+			db.close(function () {
+				assert.equal(Test.name, name);
+			});
+		});
+	});
+});

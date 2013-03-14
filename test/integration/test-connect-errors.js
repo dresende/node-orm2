bfa5d9813eb0afe45fcdb554b0c0f1e54d403930
Diogo Resende <dresende@thinkdigital.pt>
2013-03-14 15:41:06 +0000

    Changes previous commit ignoring CONNECTION_URL_NO_DATABASE for now because of sqlite (#75)

--- test/integration/test-connect-errors.js
+++ test/integration/test-connect-errors.js
@@ -3,7 +3,8 @@ var assert      = require('assert');
 
 addTest("",         "CONNECTION_URL_EMPTY");
 addTest("whatever", "CONNECTION_URL_NO_PROTOCOL");
-addTest("mysql://", "CONNECTION_URL_NO_DATABASE");
+// sqlite can be like this so this error is avoided for now
+// addTest("mysql://", "CONNECTION_URL_NO_DATABASE");
 
 function addTest(uri, error_msg) {
 	common.ORM.connect(uri, function (err) {

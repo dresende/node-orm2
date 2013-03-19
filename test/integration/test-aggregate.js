771b909e1816ca63418857ee668c8954e47807e1
Diogo Resende <dresende@thinkdigital.pt>
2013-03-19 16:05:04 +0000

    Adds test for Model.aggregate()

---
+++ test/integration/test-aggregate.js
@@ -0,0 +1,26 @@
+var common     = require('../common');
+var assert     = require('assert');
+
+common.createConnection(function (err, db) {
+	common.createModelTable('test_count', db.driver.db, function () {
+		common.insertModelData('test_count', db.driver.db, [
+			{ id : 1, name : 'test1' },
+			{ id : 2, name : 'test2' },
+			{ id : 3, name : 'test3' },
+			{ id : 4, name : 'test4' },
+			{ id : 5, name : 'test5' }
+		], function (err) {
+			if (err) throw err;
+
+			var TestModel = db.define('test_count', common.getModelProperties());
+
+			TestModel.aggregate().count('id').min('id').max('id').get(function (err, count, min, max) {
+				assert.equal(err, null);
+				assert.equal(count, 5);
+				assert.equal(min, 1);
+				assert.equal(max, 5);
+				db.close();
+			});
+		});
+	});
+});

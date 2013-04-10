84645c8acea35aa305d3f0cc3fe9d6b42662f07a
Diogo Resende <dresende@thinkdigital.pt>
2013-04-10 18:25:08 +0100

    Adds default settings properties.not_null = false (#110)

--- test/integration/test-property-types.js
+++ test/integration/test-property-types.js
@@ -1,15 +1,16 @@
 var common     = require('../common');
 var assert     = require('assert');
 var Property   = require('../../lib/Property');
+var Settings   = common.ORM.settings;
 
-assert.equal(Property.check(String).type, "text");
-assert.equal(Property.check(Number).type, "number");
-assert.equal(Property.check(Boolean).type, "boolean");
-assert.equal(Property.check(Date).type, "date");
-assert.equal(Property.check(Object).type, "object");
-assert.equal(Property.check(Buffer).type, "binary");
-assert.equal(Property.check([ 'a', 'b' ]).type, "enum");
-assert.deepEqual(Property.check([ 'a', 'b' ]).values, [ 'a', 'b' ]);
+assert.equal(Property.check(String, Settings).type, "text");
+assert.equal(Property.check(Number, Settings).type, "number");
+assert.equal(Property.check(Boolean, Settings).type, "boolean");
+assert.equal(Property.check(Date, Settings).type, "date");
+assert.equal(Property.check(Object, Settings).type, "object");
+assert.equal(Property.check(Buffer, Settings).type, "binary");
+assert.equal(Property.check([ 'a', 'b' ], Settings).type, "enum");
+assert.deepEqual(Property.check([ 'a', 'b' ], Settings).values, [ 'a', 'b' ]);
 
 assert.equal({ type: "text" }.type, "text");
 assert.equal({ type: "number" }.type, "number");
@@ -19,5 +20,5 @@ assert.equal({ type: "enum" }.type, "enum");
 assert.equal({ type: "object" }.type, "object");
 assert.equal({ type: "binary" }.type, "binary");
 
-assert.throws(function () { Property.check({ type: "buffer" }); });
-assert.throws(function () { Property.check({ type: "unknown" }); });
+assert.throws(function () { Property.check({ type: "buffer" }, Settings); });
+assert.throws(function () { Property.check({ type: "unknown" }, Settings); });

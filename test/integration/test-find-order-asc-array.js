748bb0388ffb155beca1aeff99e056b9afc4934d
Diogo Resende <dresende@thinkdigital.pt>
2013-04-15 22:26:04 +0100

    Fixes some find order test table names

--- test/integration/test-find-order-asc-array.js
+++ test/integration/test-find-order-asc-array.js
@@ -2,14 +2,14 @@ var common     = require('../common');
 var assert     = require('assert');
 
 common.createConnection(function (err, db) {
-	common.createModelTable('test_find_order', db.driver.db, function () {
-		common.insertModelData('test_find_order', db.driver.db, [
+	common.createModelTable('test_find_order_asc_array', db.driver.db, function () {
+		common.insertModelData('test_find_order_asc_array', db.driver.db, [
 			{ id : 1, name : 'test2' },
 			{ id : 2, name : 'test1' }
 		], function (err) {
 			if (err) throw err;
 
-			var TestModel = db.define('test_find_order', common.getModelProperties());
+			var TestModel = db.define('test_find_order_asc_array', common.getModelProperties());
 
 			TestModel.find([ "name" ], function (err, Instances) {
 				assert.equal(err, null);
--- test/integration/test-find-order-desc.js
+++ test/integration/test-find-order-desc.js
@@ -2,14 +2,14 @@ var common     = require('../common');
 var assert     = require('assert');
 
 common.createConnection(function (err, db) {
-	common.createModelTable('test_find_order', db.driver.db, function () {
-		common.insertModelData('test_find_order', db.driver.db, [
+	common.createModelTable('test_find_order_desc', db.driver.db, function () {
+		common.insertModelData('test_find_order_desc', db.driver.db, [
 			{ id : 1, name : 'test1' },
 			{ id : 2, name : 'test2' }
 		], function (err) {
 			if (err) throw err;
 
-			var TestModel = db.define('test_find_order', common.getModelProperties());
+			var TestModel = db.define('test_find_order_desc', common.getModelProperties());
 
 			TestModel.find("-name", function (err, Instances) {
 				assert.equal(err, null);

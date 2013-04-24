fba9012c7a5157dd594812687bbd6d7e69bd2cbf
Diogo Resende <dresende@thinkdigital.pt>
2013-04-24 11:39:33 +0100

    Fixes test-multikey-base table

--- test/integration/test-multikey-base.js
+++ test/integration/test-multikey-base.js
@@ -2,8 +2,8 @@ var common     = require('../common');
 var assert     = require('assert');
 
 common.createConnection(function (err, db) {
-	common.createKeysModelTable('test_multikey_find', db.driver.db, [ 'id1', 'id2', 'id3' ], function () {
-		common.insertKeysModelData('test_multikey_find', db.driver.db, [
+	common.createKeysModelTable('test_multikey_base', db.driver.db, [ 'id1', 'id2', 'id3' ], function () {
+		common.insertKeysModelData('test_multikey_base', db.driver.db, [
 			{ id1 : 1, id2 : 1, id3: 1, name : 'test111' },
 			{ id1 : 1, id2 : 2, id3: 3, name : 'test123' },
 			{ id1 : 2, id2 : 3, id3: 1, name : 'test231' },
@@ -11,7 +11,7 @@ common.createConnection(function (err, db) {
 		], function (err) {
 			if (err) throw err;
 
-			var TestModel = db.define('test_multikey_find', common.getModelProperties(), {
+			var TestModel = db.define('test_multikey_base', common.getModelProperties(), {
 				keys  : [ 'id1', 'id2', 'id3' ],
 				cache : false
 			});

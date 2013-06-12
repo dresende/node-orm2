16cd6bf911d291ef26559d4a615dafd4d7964ea7
Diogo Resende <dresende@thinkdigital.pt>
2013-06-12 16:34:42 +0100

    Updates test/run to show which tests are from mocha and which tests are from urun

--- test/run.js
+++ test/run.js
@@ -1,38 +1,47 @@
-var Mocha = require('mocha'),
-    fs = require('fs'),
-    path = require('path'),
-    mocha = null,
-    location = null;
-var options = {
-  include: new RegExp("integration\\/test\\-.*\\.js$")
-};
+var Mocha    = require('mocha');
+var fs       = require('fs');
+var path     = require('path');
+var location = path.normalize(path.join(__dirname, "integration2"));
+var mocha    = new Mocha({
+	reporter: "progress"
+});
+
+runTests();
 
 function runClassicTests() {
-  if (process.env.FILTER) {
-    options.include = new RegExp("integration\\/test\\-.*" + process.env.FILTER + '.*\\.js$');
-  }
-  process.stdout.write("\033[1;34m[i] \033[0;34mTesting \033[1;34m" + process.env.ORM_PROTOCOL + "\033[0m\n");
-  process.stdout.write("\033[1;34m[i] \033[0;34mURI: \033[1;34m" + require('./common').getConnectionString() + "\033[0m\n");
+	var options  = {
+		include  : new RegExp("integration\\/test\\-.*\\.js$")
+	};
 
-  require('urun')(__dirname, options);
+	if (process.env.FILTER) {
+		options.include = new RegExp("integration\\/test\\-.*" + process.env.FILTER + '.*\\.js$');
+	}
+
+	process.stdout.write("\033[1;34m[i] \033[0;34mFramework \033[1;34murun\033[0m\n");
+	process.stdout.write("\033[1;34m[i] \033[0;34mTesting \033[1;34m" + process.env.ORM_PROTOCOL + "\033[0m\n");
+	process.stdout.write("\033[1;34m[i] \033[0;34mURI: \033[1;34m" + require('./common').getConnectionString() + "\033[0m\n");
+
+	require('urun')(__dirname, options);
 }
 
-location = path.normalize(path.join(__dirname, 'integration2'));
-mocha    = new Mocha({reporter: 'nyan'});
+function runTests() {
+	fs.readdirSync(location).filter(function(file){
+		return file.substr(-3) === '.js';
+	}).forEach(function(file){
+		mocha.addFile(
+			path.join(location, file)
+		);
+	});
 
-fs.readdirSync(location).filter(function(file){
-  return file.substr(-3) === '.js';
-}).forEach(function(file){
-  mocha.addFile(
-    path.join(location, file)
-  );
-});
+	process.stdout.write("\033[1;34m[i] \033[0;34mFramework \033[1;34mmocha\033[0m\n");
+	process.stdout.write("\033[1;34m[i] \033[0;34mTesting \033[1;34m" + process.env.ORM_PROTOCOL + "\033[0m\n");
+	process.stdout.write("\033[1;34m[i] \033[0;34mURI: \033[1;34m" + require('./common').getConnectionString() + "\033[0m\n");
 
-mocha.run(function(failures){
-  if (failures > 0) {
-    process.exit(failures);
-  } else {
-    runClassicTests();
-  }
-});
-
+	mocha.run(function (failures) {
+		if (failures > 0) {
+			process.exit(failures);
+		} else {
+			runClassicTests();
+		}
+	});
+}

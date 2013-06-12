2a34e025b9ae81cbd1d0e9cda690376aa251bddf
Arek W <arek01@gmail.com>
2013-06-12 19:24:18 +1000

    Rename standardize to normalize. Hook up both test sets

--- test/integration/test-property-types.js
+++ test/integration/test-property-types.js
@@ -3,14 +3,14 @@ var assert     = require('assert');
 var Property   = require('../../lib/Property');
 var Settings   = common.ORM.settings;
 
-assert.equal(Property.standardize(String, Settings).type, "text");
-assert.equal(Property.standardize(Number, Settings).type, "number");
-assert.equal(Property.standardize(Boolean, Settings).type, "boolean");
-assert.equal(Property.standardize(Date, Settings).type, "date");
-assert.equal(Property.standardize(Object, Settings).type, "object");
-assert.equal(Property.standardize(Buffer, Settings).type, "binary");
-assert.equal(Property.standardize([ 'a', 'b' ], Settings).type, "enum");
-assert.deepEqual(Property.standardize([ 'a', 'b' ], Settings).values, [ 'a', 'b' ]);
+assert.equal(Property.normalize(String, Settings).type, "text");
+assert.equal(Property.normalize(Number, Settings).type, "number");
+assert.equal(Property.normalize(Boolean, Settings).type, "boolean");
+assert.equal(Property.normalize(Date, Settings).type, "date");
+assert.equal(Property.normalize(Object, Settings).type, "object");
+assert.equal(Property.normalize(Buffer, Settings).type, "binary");
+assert.equal(Property.normalize([ 'a', 'b' ], Settings).type, "enum");
+assert.deepEqual(Property.normalize([ 'a', 'b' ], Settings).values, [ 'a', 'b' ]);
 
 assert.equal({ type: "text" }.type, "text");
 assert.equal({ type: "number" }.type, "number");
@@ -20,5 +20,5 @@ assert.equal({ type: "enum" }.type, "enum");
 assert.equal({ type: "object" }.type, "object");
 assert.equal({ type: "binary" }.type, "binary");
 
-assert.throws(function () { Property.standardize({ type: "buffer" }, Settings); });
-assert.throws(function () { Property.standardize({ type: "unknown" }, Settings); });
+assert.throws(function () { Property.normalize({ type: "buffer" }, Settings); });
+assert.throws(function () { Property.normalize({ type: "unknown" }, Settings); });

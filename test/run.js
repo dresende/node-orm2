e91c52b6f502be2d6d83392fd99a00cb1cc248ab
Arek W <arek01@gmail.com>
2013-06-12 20:05:26 +1000

    Minor test runner cleanup

--- test/run.js
+++ test/run.js
@@ -6,11 +6,9 @@ var Mocha = require('mocha'),
 var options = {};
 
 function runClassicTests() {
-  // Classical tests
   if (process.env.FILTER) {
     options.include = new RegExp(process.env.FILTER + '.*\\.js$');
   }
-
   process.stdout.write("\033[1;34m[i] \033[0;34mTesting \033[1;34m" + process.env.ORM_PROTOCOL + "\033[0m\n");
   process.stdout.write("\033[1;34m[i] \033[0;34mURI: \033[1;34m" + require('./common').getConnectionString() + "\033[0m\n");
 

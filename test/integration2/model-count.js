79574e96e309ce94afcd7a8d96622e2e2d5847d4
Diogo Resende <dresende@thinkdigital.pt>
2013-06-13 00:13:06 +0100

    Adds new test for Model.count(), deprecates old one

---
+++ test/integration2/model-count.js
@@ -0,0 +1,71 @@
+var _        = require('lodash');
+var should   = require('should');
+var helper   = require('../support/spec_helper');
+var async    = require('async');
+var ORM      = require('../../');
+
+describe("Model.count()", function() {
+	var db = null;
+	var Person = null;
+
+	var setup = function () {
+		return function (done) {
+			Person = db.define("person", {
+				name   : String
+			});
+
+			return helper.dropSync(Person, function () {
+				Person.create([{
+					id  : 1,
+					name: "John Doe"
+				}, {
+					id  : 2,
+					name: "Jane Doe"
+				}, {
+					id  : 3,
+					name: "John Doe"
+				}], done);
+			});
+		};
+	};
+
+	before(function (done) {
+		helper.connect(function (connection) {
+			db = connection;
+
+			return done();
+		});
+	});
+
+	after(function () {
+		return db.close();
+	});
+
+	describe("without conditions", function () {
+		before(setup());
+
+		it("should return all items in model", function (done) {
+			Person.count(function (err, count) {
+				should.equal(err, null);
+
+				count.should.equal(3);
+
+				return done();
+			});
+		});
+	});
+
+	describe("with conditions", function () {
+		before(setup());
+
+		it("should return only matching items", function (done) {
+			Person.count({ name: "John Doe" }, function (err, count) {
+				should.equal(err, null);
+
+				count.should.equal(2);
+
+				return done();
+			});
+		});
+	});
+});

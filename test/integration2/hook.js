432ac6b13e0b2f39179f2fc2f4e2629a5b149c48
Diogo Resende <dresende@thinkdigital.pt>
2013-06-12 22:13:58 +0100

    Adds hook tests, deprecates old ones

---
+++ test/integration2/hook.js
@@ -0,0 +1,313 @@
+var _        = require('lodash');
+var should   = require('should');
+var helper   = require('../support/spec_helper');
+var async    = require('async');
+var ORM      = require('../../');
+
+describe("Hook", function() {
+	var db = null;
+	var Person = null;
+
+	var triggeredHooks = {};
+
+	var checkHook = function (hook) {
+		triggeredHooks[hook] = false;
+
+		return function () {
+			triggeredHooks[hook] = Date.now();
+		};
+	};
+
+	var setup = function (hooks) {
+		if (typeof hooks == "undefined") {
+			hooks = {
+				afterCreate      : checkHook("afterCreate"),
+				beforeCreate     : checkHook("beforeCreate"),
+				afterSave        : checkHook("afterSave"),
+				beforeSave       : checkHook("beforeSave"),
+				beforeValidation : checkHook("beforeValidation"),
+				beforeRemove     : checkHook("beforeRemove"),
+				afterRemove      : checkHook("afterRemove")
+			};
+		}
+
+		return function (done) {
+			Person = db.define("person", {
+				name   : String
+			}, {
+				hooks  : hooks
+			});
+
+			return helper.dropSync(Person, done);
+		};
+	};
+
+	before(function (done) {
+		helper.connect(function (connection) {
+			db = connection;
+
+			return done();
+		});
+	});
+
+	after(function () {
+		return db.close();
+	});
+
+	describe("beforeCreate", function () {
+		before(setup());
+
+		it("should trigger before creating instance", function (done) {
+			Person.create([{ name: "John Doe" }], function () {
+				triggeredHooks.afterCreate.should.be.a("number");
+				triggeredHooks.beforeCreate.should.be.a("number");
+				triggeredHooks.beforeCreate.should.not.be.above(triggeredHooks.afterCreate);
+
+				return done();
+			});
+		});
+
+		describe("when setting properties", function () {
+			before(setup({
+				beforeCreate : function () {
+					this.name = "Jane Doe";
+				}
+			}));
+
+			it("should not be discarded", function (done) {
+				Person.create([{ }], function (err, items) {
+					should.equal(err, null);
+
+					items.should.be.a("object");
+					items.should.have.property("length", 1);
+					items[0].name.should.equal("Jane Doe");
+
+					return done();
+				});
+			});
+		});
+
+		describe("if hook method has 1 argument", function () {
+			var beforeCreate = false;
+
+			before(setup({
+				beforeCreate : function (next) {
+					setTimeout(function () {
+						beforeCreate = true;
+
+						return next();
+					}.bind(this), 200);
+				}
+			}));
+
+			it("should wait for hook to finish", function (done) {
+				this.timeout(500);
+
+				Person.create([{ name: "John Doe" }], function () {
+					beforeCreate.should.be.true;
+
+					return done();
+				});
+
+			});
+		});
+	});
+
+	describe("afterCreate", function () {
+		before(setup());
+
+		it("should trigger after creating instance", function (done) {
+			Person.create([{ name: "John Doe" }], function () {
+				triggeredHooks.afterCreate.should.be.a("number");
+				triggeredHooks.beforeCreate.should.be.a("number");
+				triggeredHooks.afterCreate.should.not.be.below(triggeredHooks.beforeCreate);
+
+				return done();
+			});
+		});
+	});
+
+	describe("beforeSave", function () {
+		before(setup());
+
+		it("should trigger before saving an instance", function (done) {
+			Person.create([{ name: "John Doe" }], function () {
+				triggeredHooks.afterSave.should.be.a("number");
+				triggeredHooks.beforeSave.should.be.a("number");
+				triggeredHooks.beforeSave.should.not.be.above(triggeredHooks.afterSave);
+
+				return done();
+			});
+		});
+
+		describe("if hook method has 1 argument", function () {
+			var beforeSave = false;
+
+			before(setup({
+				beforeSave : function (next) {
+					setTimeout(function () {
+						beforeSave = true;
+
+						return next();
+					}.bind(this), 200);
+				}
+			}));
+
+			it("should wait for hook to finish", function (done) {
+				this.timeout(500);
+
+				Person.create([{ name: "John Doe" }], function () {
+					beforeSave.should.be.true;
+
+					return done();
+				});
+
+			});
+		});
+	});
+
+	describe("afterSave", function () {
+		before(setup());
+
+		it("should trigger after saving an instance", function (done) {
+			Person.create([{ name: "John Doe" }], function () {
+				triggeredHooks.afterSave.should.be.a("number");
+				triggeredHooks.beforeSave.should.be.a("number");
+				triggeredHooks.afterSave.should.not.be.below(triggeredHooks.beforeSave);
+
+				return done();
+			});
+		});
+	});
+
+	describe("beforeValidation", function () {
+		before(setup());
+
+		it("should trigger before instance validation", function (done) {
+			Person.create([{ name: "John Doe" }], function () {
+				triggeredHooks.beforeValidation.should.be.a("number");
+				triggeredHooks.beforeCreate.should.be.a("number");
+				triggeredHooks.beforeSave.should.be.a("number");
+				triggeredHooks.beforeValidation.should.not.be.above(triggeredHooks.beforeCreate);
+				triggeredHooks.beforeValidation.should.not.be.above(triggeredHooks.beforeSave);
+
+				return done();
+			});
+		});
+
+		describe("if hook method has 1 argument", function () {
+			var beforeValidation = false;
+
+			before(setup({
+				beforeValidation : function (next) {
+					setTimeout(function () {
+						beforeValidation = true;
+
+						if (!this.name) return next("Name is missing");
+
+						return next();
+					}.bind(this), 200);
+				}
+			}));
+
+			it("should wait for hook to finish", function (done) {
+				this.timeout(500);
+
+				Person.create([{ name: "John Doe" }], function () {
+					beforeValidation.should.be.true;
+
+					return done();
+				});
+			});
+
+			it("should trigger error if hook passes an error", function (done) {
+				this.timeout(500);
+
+				Person.create([{ name: "" }], function (err) {
+					beforeValidation.should.be.true;
+
+					err.should.equal("Name is missing");
+
+					return done();
+				});
+			});
+		});
+	});
+
+	describe("afterLoad", function () {
+		var afterLoad = false;
+
+		before(setup({
+			afterLoad: function () {
+				afterLoad = true;
+			}
+		}));
+
+		it("should trigger when defining a model", function (done) {
+			var John = new Person({ name: "John" });
+
+			afterLoad.should.be.true;
+
+			return done();
+		});
+	});
+
+	describe("beforeRemove", function () {
+		before(setup());
+
+		it("should trigger before removing an instance", function (done) {
+			Person.create([{ name: "John Doe" }], function (err, items) {
+				items[0].remove(function () {
+					triggeredHooks.afterRemove.should.be.a("number");
+					triggeredHooks.beforeRemove.should.be.a("number");
+					triggeredHooks.beforeRemove.should.not.be.above(triggeredHooks.afterRemove);
+
+					return done();
+				});
+			});
+		});
+
+		describe("if hook method has 1 argument", function () {
+			var beforeRemove = false;
+
+			before(setup({
+				beforeRemove : function (next) {
+					setTimeout(function () {
+						beforeRemove = true;
+
+						return next();
+					}.bind(this), 200);
+				}
+			}));
+
+			it("should wait for hook to finish", function (done) {
+				this.timeout(500);
+
+				Person.create([{ name: "John Doe" }], function (err, items) {
+					items[0].remove(function () {
+						beforeRemove.should.be.true;
+
+						return done();
+					});
+				});
+
+			});
+		});
+	});
+
+	describe("afterRemove", function () {
+		before(setup());
+
+		it("should trigger after removing an instance", function (done) {
+			Person.create([{ name: "John Doe" }], function (err, items) {
+				items[0].remove(function () {
+					triggeredHooks.afterRemove.should.be.a("number");
+					triggeredHooks.beforeRemove.should.be.a("number");
+					triggeredHooks.afterRemove.should.not.be.below(triggeredHooks.beforeRemove);
+
+					return done();
+				});
+			});
+		});
+	});
+});

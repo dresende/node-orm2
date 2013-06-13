add9ef47a1e46b314a5c8b2563d27a37fce17ade
Diogo Resende <dresende@thinkdigital.pt>
2013-06-13 14:50:59 +0100

    Adds new test for Model.save(), deprecates old ones

---
+++ test/integration2/model-save.js
@@ -0,0 +1,127 @@
+var _        = require('lodash');
+var should   = require('should');
+var helper   = require('../support/spec_helper');
+var async    = require('async');
+var ORM      = require('../../');
+
+describe("Model.save()", function() {
+	var db = null;
+	var Person = null;
+
+	var setup = function () {
+		return function (done) {
+			Person = db.define("person", {
+				name   : String
+			});
+			Person.hasOne("parent");
+
+			return helper.dropSync(Person, done);
+		};
+	};
+
+	before(function (done) {
+		helper.connect(function (connection) {
+			db = connection;
+
+			return done();
+		});
+	});
+
+	after(function () {
+		return db.close();
+	});
+
+	describe("with callback", function () {
+		before(setup());
+
+		it("should save item and return id", function (done) {
+			var John = new Person({
+				name: "John"
+			});
+			John.save(function (err) {
+				should.equal(err, null);
+				John.id.should.be.a("number");
+
+				Person.get(John.id, function (err, JohnCopy) {
+					should.equal(err, null);
+
+					JohnCopy.id.should.equal(John.id);
+					JohnCopy.name.should.equal(John.name);
+
+					return done();
+				});
+			});
+		});
+	});
+
+	describe("with properties object", function () {
+		before(setup());
+
+		it("should update properties, save item and return id", function (done) {
+			var John = new Person({
+				name: "Jane"
+			});
+			John.save({ name: "John" }, function (err) {
+				should.equal(err, null);
+				John.id.should.be.a("number");
+				John.name.should.equal("John");
+
+				Person.get(John.id, function (err, JohnCopy) {
+					should.equal(err, null);
+
+					JohnCopy.id.should.equal(John.id);
+					JohnCopy.name.should.equal(John.name);
+
+					return done();
+				});
+			});
+		});
+	});
+
+	describe("if passed an association instance", function () {
+		before(setup());
+
+		it("should save association first and then save item and return id", function (done) {
+			var Jane = new Person({
+				name  : "Jane"
+			});
+			var John = new Person({
+				name  : "John",
+				parent: Jane
+			});
+			John.save(function (err) {
+				should.equal(err, null);
+				John.saved().should.be.true;
+				Jane.saved().should.be.true;
+
+				John.id.should.be.a("number");
+				Jane.id.should.be.a("number");
+
+				return done();
+			});
+		});
+	});
+
+	describe("if passed an association object", function () {
+		before(setup());
+
+		it("should save association first and then save item and return id", function (done) {
+			var John = new Person({
+				name  : "John",
+				parent: {
+					name  : "Jane"
+				}
+			});
+			John.save(function (err) {
+				should.equal(err, null);
+				John.saved().should.be.true;
+				John.parent.saved().should.be.true;
+
+				John.id.should.be.a("number");
+				John.parent.id.should.be.a("number");
+
+				return done();
+			});
+		});
+	});
+});

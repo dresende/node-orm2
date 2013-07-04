b2c80a51977843f16a481ce895adb489b9d9617c
Diogo Resende <dresende@thinkdigital.pt>
2013-07-04 19:02:21 +0100

    Reindents Model.hasOne header declarations

--- test/integration2/association-hasone.js
+++ test/integration2/association-hasone.js
@@ -1,8 +1,8 @@
-var _				= require('lodash');
-var should	 = require('should');
-var helper	 = require('../support/spec_helper');
-var async		= require('async');
-var ORM			= require('../../');
+var ORM    = require('../../');
+var helper = require('../support/spec_helper');
+var should = require('should');
+var async  = require('async');
+var _      = require('lodash');
 
 describe("hasOne", function() {
 	var db    = null;

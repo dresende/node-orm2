8b5ba97c518a2cecab4324575faf99026d65d3f5
Diogo Resende <dresende@thinkdigital.pt>
2013-07-04 01:03:51 +0100

    Fixes passing json object instead of instances to Model.create() associations (#216)
    Adds initial tests for Model.create(), deprecates old one

---
+++ test/integration2/model-create.js
@@ -0,0 +1,113 @@
+var should   = require('should');
+var helper   = require('../support/spec_helper');
+var ORM      = require('../../');
+
+describe("Model.create()", function() {
+	var db = null;
+	var Pet = null;
+	var Person = null;
+
+	var setup = function () {
+		return function (done) {
+			Person = db.define("person", {
+				name   : String
+			});
+			Pet = db.define("pet", {
+				name   : String
+			});
+			Person.hasMany("pets", Pet);
+
+			return helper.dropSync([ Person, Pet ], done);
+		};
+	};
+
+	before(function (done) {
+		helper.connect(function (connection) {
+			db = connection;
+
+			return done();
+		});
+	});
+
+	after(function () {
+		return db.close();
+	});
+
+	describe("if passing an object", function () {
+		before(setup());
+
+		it("should accept it as the only item to create", function (done) {
+			Person.create({
+				name : "John Doe"
+			}, function (err, John) {
+				should.equal(err, null);
+				John.should.have.property("name", "John Doe");
+
+				return done();
+			});
+		});
+	});
+
+	describe("if passing an array", function () {
+		before(setup());
+
+		it("should accept it as a list of items to create", function (done) {
+			Person.create([{
+				name : "John Doe"
+			}, {
+				name : "Jane Doe"
+			}], function (err, people) {
+				should.equal(err, null);
+				should(Array.isArray(people));
+
+				people.should.have.property("length", 2);
+				people[0].should.have.property("name", "John Doe");
+				people[1].should.have.property("name", "Jane Doe");
+
+				return done();
+			});
+		});
+	});
+
+	describe("if element has an association", function () {
+		before(setup());
+
+		it("should also create it or save it", function (done) {
+			Person.create({
+				name : "John Doe",
+				pets : [ new Pet({ name: "Deco" }) ]
+			}, function (err, John) {
+				should.equal(err, null);
+
+				John.should.have.property("name", "John Doe");
+
+				should(Array.isArray(John.pets));
+
+				John.pets[0].should.have.property("name", "Deco");
+				John.pets[0].should.have.property("id");
+				John.pets[0].saved().should.be.true;
+
+				return done();
+			});
+		});
+
+		it("should also create it or save it even if it's an object and not an instance", function (done) {
+			Person.create({
+				name : "John Doe",
+				pets : [ { name: "Deco" } ]
+			}, function (err, John) {
+				should.equal(err, null);
+
+				John.should.have.property("name", "John Doe");
+
+				should(Array.isArray(John.pets));
+
+				John.pets[0].should.have.property("name", "Deco");
+				John.pets[0].should.have.property("id");
+				John.pets[0].saved().should.be.true;
+
+				return done();
+			});
+		});
+	});
+});

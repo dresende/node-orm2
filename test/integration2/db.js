23f65ba14d6045995464078304fb3a81e15494dc
Diogo Resende <dresende@thinkdigital.pt>
2013-07-11 00:08:50 +0100

    Moves test-db-use to new framework

--- test/integration2/db.js
+++ test/integration2/db.js
@@ -2,6 +2,48 @@ var should   = require('should');
 var helper   = require('../support/spec_helper');
 var ORM      = require('../../');
 
+describe("db.use()", function () {
+	var db = null;
+
+	before(function (done) {
+		helper.connect(function (connection) {
+			db = connection;
+
+			return done();
+		});
+	});
+
+	after(function () {
+		return db.close();
+	});
+
+	it("should be able to register a plugin", function (done) {
+		var MyPlugin = function MyPlugin(DB, opts) {
+			db.should.equal(DB);
+			opts.should.eql({ option: true });
+
+			return {
+				define: function (Model) {
+					Model.should.be.a("function");
+					Model.id.should.be.a("string");
+					calledDefine = true;
+				}
+			};
+		};
+
+		db.use(MyPlugin, { option: true });
+
+		var calledDefine = false;
+		var MyModel = db.define("my_model", { // db.define should call plugin.define method
+			property: String
+		});
+
+		calledDefine.should.be.true;
+
+		return done();
+	});
+});
+
 describe("db.define()", function() {
 	var db = null;
 

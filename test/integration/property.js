6610605da4a1c4079456c7326b30829b9bc72afb
Arek W <arek01@gmail.com>
2014-05-08 20:26:30 +1000

    Fix duplicate column issue with orm-timestamps

--- test/integration/property.js
+++ test/integration/property.js
@@ -3,82 +3,67 @@ var ORM      = require("../..");
 var Property = ORM.Property;
 
 describe("Property", function () {
-	describe("passing String", function() {
-		it("should return type: 'text'", function (done) {
-			Property.normalize(
-				{ prop: String, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("text");
+	it("passing String should return type: 'text'", function (done) {
+		Property.normalize(
+			{ prop: String, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("text");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing Number", function() {
-		it("should return type: 'number'", function (done) {
-			Property.normalize(
-				{ prop: Number, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("number");
+	it("passing Number should return type: 'number'", function (done) {
+		Property.normalize(
+			{ prop: Number, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("number");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing deprecated rational: false number", function() {
-		it("should return type: 'integer'", function (done) {
-			Property.normalize(
-				{ prop: {type: 'number', rational: false}, customTypes: {}, settings: ORM.settings, name: 'abc'}
-			).type.should.equal("integer");
+	it("passing deprecated rational: false number should return type: 'integer'", function (done) {
+		Property.normalize(
+			{ prop: {type: 'number', rational: false}, customTypes: {}, settings: ORM.settings, name: 'abc'}
+		).type.should.equal("integer");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing Boolean", function() {
-		it("should return type: 'boolean'", function (done) {
-			Property.normalize(
-				{ prop: Boolean, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("boolean");
 
-			return done();
-		});
+	it("passing Boolean should return type: 'boolean'", function (done) {
+		Property.normalize(
+			{ prop: Boolean, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("boolean");
+
+		return done();
 	});
-	describe("passing Date", function() {
-		it("should return type: 'date'", function (done) {
-			Property.normalize(
-				{ prop: Date, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("date");
+	it("passing Date should return type: 'date'", function (done) {
+		Property.normalize(
+			{ prop: Date, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("date");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing Object", function() {
-		it("should return type: 'object'", function (done) {
-			Property.normalize(
-				{ prop: Object, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("object");
+	it("passing Object should return type: 'object'", function (done) {
+		Property.normalize(
+			{ prop: Object, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("object");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing Buffer", function() {
-		it("should return type: 'binary'", function (done) {
-			Property.normalize(
-				{ prop: Buffer, customTypes: {}, settings: ORM.settings, name: 'abc' }
-			).type.should.equal("binary");
+	it("passing Buffer should return type: 'binary'", function (done) {
+		Property.normalize(
+			{ prop: Buffer, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		).type.should.equal("binary");
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing an Array of items", function() {
-		it("should return type: 'enum' with list of items", function (done) {
-			var prop = Property.normalize(
-				{ prop: [1, 2, 3], customTypes: {}, settings: ORM.settings, name: 'abc' }
-			)
+	it("passing an Array of items should return type: 'enum' with list of items", function (done) {
+		var prop = Property.normalize(
+			{ prop: [1, 2, 3], customTypes: {}, settings: ORM.settings, name: 'abc' }
+		)
 
-			prop.type.should.equal("enum");
-			prop.values.should.have.property("length", 3);
+		prop.type.should.equal("enum");
+		prop.values.should.have.property("length", 3);
 
-			return done();
-		});
+		return done();
 	});
-	describe("passing a string type", function() {
+	describe("passing a string type", function () {
 		it("should return type: <type>", function (done) {
 			Property.normalize(
 				{ prop: "text", customTypes: {}, settings: ORM.settings, name: 'abc' }
@@ -86,7 +71,7 @@ describe("Property", function () {
 
 			return done();
 		});
-    it("should accept: 'point'", function(done) {
+    it("should accept: 'point'", function (done) {
       Property.normalize(
 				{ prop: "point", customTypes: {}, settings: ORM.settings, name: 'abc' }
 			).type.should.equal("point");
@@ -105,5 +90,17 @@ describe("Property", function () {
 				return done();
 			});
 		});
+	});
+	it("should not modify the original property object", function (done) {
+		var original = { type: 'text', required: true };
+
+		var normalized = Property.normalize(
+			{ prop: original, customTypes: {}, settings: ORM.settings, name: 'abc' }
+		);
+
+		original.test = 3;
+		should.strictEqual(normalized.test, undefined);
+
+		return done();
 	});
 });
